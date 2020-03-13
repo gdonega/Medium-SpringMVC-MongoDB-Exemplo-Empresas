81b6ee0c8338b8e3a62db777643e303d3e26acae
gdonega <gustavo.dq.27@hotmail.com>
2020-03-13 00:09:52 -0300

    up

--- com.testes.spring.exemplo.empresas/src/com/testes/spring/exemplo/empresas/services/EmpresaEnvelopeService.java
+++ com.testes.spring.exemplo.empresas/src/com/testes/spring/exemplo/empresas/services/EmpresaEnvelopeService.java
@@ -3,17 +3,11 @@ package com.testes.spring.exemplo.empresas.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.SortOperation;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.mongodb.BasicDBObject;
+import com.testes.spring.exemplo.empresas.daos.EmpresaEnvelopeDAO;
 import com.testes.spring.exemplo.empresas.dtos.MunicipioGroupDto;
 import com.testes.spring.exemplo.empresas.models.EmpresaEnvelope;
 import com.testes.spring.exemplo.empresas.repositories.EmpresaEnvelopeRepository;
@@ -25,7 +19,7 @@ public class EmpresaEnvelopeService {
 	private EmpresaEnvelopeRepository empresaEnvelopeRepository;
 
 	@Autowired
-	private MongoOperations mongoTemplate;
+	private EmpresaEnvelopeDAO empresaEnvelopeDAO;
 
 	public void salvarCnpj(String cnpj) {
 		/*---------------------- Fazendo a requisição ----------------------*/
@@ -53,31 +47,6 @@ public class EmpresaEnvelopeService {
 	}
 
 	public List<MunicipioGroupDto> agruparPorMunicipio() {
-
-		// Define a operação de agregação -> {$group:{_id: "$municipio", count: { $sum:
-		// 1 }}}
-		GroupOperation municipioGroupOperation = Aggregation.group("municipio").count().as("count");
-
-		// Define a operação de ordenação -> {$sort: { _id: 1}}
-		SortOperation municipioSortOperation = Aggregation.sort(Direction.ASC, "_id");
-
-		// Adiciona todas as operações na lista de AggregationOperation.
-		// A ordem IMPORTA.
-		AggregationOperation[] agregacoes = new AggregationOperation[] { municipioGroupOperation,
-				municipioSortOperation };
-
-		// Gera o objecto agggregation que como base o
-		// AggregationOperation[]. Esse objeto é usado para executar a
-		// pesquisa
-		Aggregation aggregation = Aggregation.newAggregation(agregacoes);
-
-		// Executa a query no banco de dados, e faz o cast para um objeto
-		// genérico
-		AggregationResults<MunicipioGroupDto> output = mongoTemplate.aggregate(aggregation, "empresas",
-				MunicipioGroupDto.class);
-
-		// Recupera o valor da propriedade "cotaExcedida"
-		return output.getMappedResults();
-
+		return empresaEnvelopeDAO.agruparPorMunicipio();
 	}
 }
